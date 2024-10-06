89492a9c3be5cb4ab736ac3f7c67d25dffa3c5aa
= <=>
2024-10-06 19:01:20 -0400

    did things

--- commands/marketplace/auction.js
+++ commands/marketplace/auction.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
 const https = require("https")
-const fs = require('node:fs');
+const { writeFileSync } = require('node:fs');
+
+const JSONToFile = (obj, filename) =>
+    writeFileSync(`${filename}.json`, JSON.stringify(obj, null, 2));
 
 module.exports = {
 	category: 'marketplace',
@@ -13,11 +16,11 @@ module.exports = {
             .setDescription("Get all available auctions")
     ),
     async execute(interaction){
-        fs.writeFileSync('../auctionCache.json', "hello");
+        JSONToFile({"text":"hello"}, "../auctionCache")
         const { cacheAge, auctions } = require("../../auctionCache.json");
         if(Date.now() >= cacheAge + 30000){ // Update info once every 30 seconds, only when prompted.
-            fetch("https://nandertga.ddns.net:4097/api/v2/auctions").then((listings) =>{
-                fs.writeFileSync('../auctionCache.json', "hello");
+            fetch("https://nandertga.ddns.net:4097/api/v2/auctions").then(res => res.json()).then((listings) =>{
+                console.log(listings[0].ended)
             });
             const { auctions } = require("../../auctionCache.json");
             console.log(`User ${interaction.user.tag} refreshed cache`);
