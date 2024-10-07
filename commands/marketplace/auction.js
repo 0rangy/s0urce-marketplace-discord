4aca5afc77e008e035e2453d8bceb11ebc374644
= <=>
2024-10-07 08:02:21 -0400

    Fully implemented cache refreshing

--- commands/marketplace/auction.js
+++ commands/marketplace/auction.js
@@ -13,23 +13,26 @@ module.exports = {
             .setDescription("Get all available auctions")
     ),
     async execute(interaction){
-        const { cacheAge, auctions } = require("../../auctionCache.json");
-        if(Date.now() >= cacheAge + 30000){ // Update info once every 30 seconds, only when prompted.
+        const data = fs.readFileSync('./auctionCache.json',
+            { encoding: 'utf8', flag: 'r' });
+        dataParsed = JSON.parse(data);
+        const timeDif = Date.now()/1000 - dataParsed.cacheAge;
+        if(timeDif >= 30){ // Update info once every 30 seconds, only when prompted.
             fetch("https://nandertga.ddns.net:4097/api/v2/auctions").then(res => res.json()).then((listings) =>{
                 console.log(Date.now())
                 fs.writeFileSync('./auctionCache.json', JSON.stringify({
-                        "cacheAge": Date.now(),
+                        "cacheAge": Date.now()/1000,
                         "auctions": listings
                     },null, 2), {
                     encoding: "utf8",
                     mode: 0o666
                   })
             });
-            const { cacheAge, auctions } = require("../../auctionCache.json");
-            console.log(`User ${interaction.user.tag} refreshed cache. (${Date.now() - (cacheAge + 30000)})`);
+            
+            console.log(`User ${interaction.user.tag} refreshed cache. (${timeDif})`);
         }
+        console.log(`${timeDif}`)
 
-
-        return interaction.reply(auctions[0].name);
+        return interaction.reply(dataParsed.auctions[0].name);
     }
 }
